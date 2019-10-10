80b192be78e38db6c25e4c4e2c9708effad68ba7
diego.bezerra <diego.bezerra@gprt.ufpe.br>
2019-10-10 12:13:28 -0300

    Update repository.

--- 2016.1/controller/compile.sh
+++
@@ -1,3 +0,0 @@
-TARGETDIR="global2"
-gcc -I global_controller_c -I /usr/local/lib/heptagon/c -c -o $TARGETDIR"_c"/_main.o $TARGETDIR"_c"/_main.c
-gcc -o global_sim $TARGETDIR"_c"/_main.o $TARGETDIR"_c"/$TARGETDIR.o /usr/lib/x86_64-linux-gnu/libmicrohttpd.so global_controller_c/global_controller.o
